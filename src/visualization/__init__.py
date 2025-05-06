1704434649890c3ae06e0ec9749372d1f14eb16b
Kissyfur <sheick_link@hotmail.com>
2025-05-06 17:14:25 +0200

    visualization functions

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -1,6 +1,40 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
+
+def plot_heat_map(data, col_name, title='', label='', log_scale_bar=True, save=None):
+    # Create a figure with a map projection
+    fig = plt.figure(figsize=(10, 8))
+    ax = plt.axes(projection=ccrs.Mercator())  # You can choose different projections like 'PlateCarree'
+
+    # Add coastlines and other features
+    ax.coastlines()
+    ax.add_feature(cfeature.BORDERS)
+    ax.add_feature(cfeature.LAND, zorder=10, edgecolor='black')
+    ax.add_feature(cfeature.LAKES, edgecolor='black')
+    ax.add_feature(cfeature.RIVERS)
+
+    # Set the extent for the Mediterranean Sea
+    ax.set_extent([-6., 36, 30, 46], crs=ccrs.PlateCarree())  # [west, east, south, north]
+
+    # Plot the data as a scatter plot
+    sc = ax.scatter(data['lon'], data['lat'], c=data[col_name], cmap='viridis', s=0.001, transform=ccrs.PlateCarree(),
+                    norm=matplotlib.colors.LogNorm() if log_scale_bar else None)
+
+    # Add a colorbar
+    cbar = plt.colorbar(sc, orientation='vertical', pad=0.02, shrink=0.5)
+    cbar.set_label(label)
+
+    # Add title and labels
+    plt.title(title, fontsize=16)
+
+    # Show or save the plot
+    plt.savefig(save, dpi=300) if save is not None else None
+    # plt.show()
+    plt.close()
 
 def plot_lines(lines, title, save=None):
     # max_y = max([len(line) for line_name, line in lines.items()])
